8791224083c06c46a992e1c30d89ce458d03a095
Chris Glerup <cglerupcoding@gmail.com>
2019-05-31 14:31:52 -0500

    added query to products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,3 +16,22 @@ connection.connect(function(err) {
     if (err) throw err;
 });
 
+// Query to products
+connection.query('SELECT * FROM products', function(err, res) {
+    if (err) throw err;
+    
+    console.log("Item # | Product -- Department -- Price -- Quantity ");
+
+    // loop through rows
+    for (var i=0; i<res.length; i++){
+    	if (i<9){
+    		console.log(" " + res[i].Item_ID + "     | " + res[i].Product_Name + " -- " + res[i].Department_Name + "--" + res[i].Price + "--" + res[i].Stock_Quantity);
+    	}
+    	else if (i >= 9){
+    		console.log(res[i].Item_ID + "     | " + res[i].Product_Name + " -- " + res[i].Department_Name + "--" + res[i].Price + "--" + res[i].Stock_Quantity);
+    	}
+    }
+    
+    promptUser();
+});
+
