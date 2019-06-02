b21e8acd8239a463f84091d485937b0d1ff1e3f9
Chris Glerup <cglerupcoding@gmail.com>
2019-06-01 22:58:26 -0500

    code complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -32,3 +32,77 @@ connection.query('SELECT * FROM products', function(err, res) {
     	}
     }
     
+    promptUser();
+});
+var promptUser = function(){
+
+    // User Prompt
+	inquirer.prompt([{
+		name: "Item_ID",
+		message: "Please Select your Quanity",
+
+    
+		validate: function(value){
+            if (isNaN(value) == false) {
+                return true;
+            }
+            else {
+            	return false;
+            }
+		}
+    },
+    
+    {
+
+      
+        name: "userQuantity",
+        message: "Please Select your Quanity?",
+
+        
+        validate: function(value){
+            if (isNaN(value) == false) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+       
+    }]).then(function(answers){
+
+         
+    		var currentItem = answers.Item_ID;
+    		var currentAmount = answers.userQuantity;
+            connection.query('SELECT * FROM products WHERE ?',{
+                Item_ID: answers.Item_ID
+            },function(err, res){
+
+       
+                if (currentAmount > res[0].Stock_Quantity){
+                    console.log("You cannot buy that many!");
+
+              
+                    promptUser();
+                }
+               
+                else { 
+                    console.log("You can buy it!");
+
+        
+                    var newQuantity = (res[0].Stock_Quantity - currentAmount);
+                    var totalCost = res[0].Price*currentAmount;
+
+                    
+                    connection.query('UPDATE products SET ? WHERE ?',[{
+                        Stock_Quantity: newQuantity
+                    },{
+                        Item_ID: currentItem
+                    }], function(err, res){
+                        console.log("You were charged $" + totalCost);
+
+                        promptUser();
+                    });
+                }
+            })
+	   })
+}   
